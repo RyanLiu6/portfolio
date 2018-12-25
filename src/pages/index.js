90e6d41a4d0ff42e9553de09a8e145b1227eea1d
Ryan Liu <dio.ryanliu@hotmail.com>
2018-12-24 23:30:47 -0800

    Updating some stuff yo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import ContactLinks from '../components/ContactLinks'
+import Footer from '../components/Footer'
+
+import projects from '../content/projects'
+import statements from '../content/about-me'
 
 export default class Home extends React.PureComponent {
-  render() {
+  render () {
     return (
       <div className="home">
+        <Navbar />
+        <Hero />
+        <About statements={statements} />
         <h2 id="projects" className="section">
           Projects
         </h2>
+        <Projects projects={projects} />
         <div className="background" />
         <h2 id="contact" className="section">
           Hit me up!
         </h2>
+        <ContactLinks />
+        <Footer />
       </div>
-    );
+    )
   }
 }
